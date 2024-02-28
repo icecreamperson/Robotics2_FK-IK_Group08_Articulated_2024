f56a9f09723ac5f0d5f2b6d89e1191a829340837
Dannielle Louis Abeleda <157472473+icecreamperson@users.noreply.github.com>
2024-02-28 16:50:36 +0800

    Update ArtiIK.py

--- Inverse Kinematics/ArtiIK.py
+++ Inverse Kinematics/ArtiIK.py
@@ -1,31 +1,36 @@
 import numpy as np
 import math
 
-#Inverse Kinematics Using Graphical Method
-
-#link lengths in mm
 a1 = float(input("a1 = "))
 a2 = float(input("a2 = "))
 a3 = float(input("a3 = "))
 
-#Position Vector in mm
 xe = float(input("X = "))
 ye = float(input("Y = "))
 ze = float(input("Z = "))
 
-Th1 = np.arctan(ye/xe) #1
+if xe == 0:
+    Th1 = np.pi/2 * 180/np.pi if ye > 0 else -np.pi/2
+else:
+    Th1 = np.arctan(ye/xe) * 180/np.pi #1
+
 r1 = np.sqrt(ye**2 + xe**2) #2
 r2 = ze - a1 #3
-phi1 = np.arctan(r2/r1) #4
 
-r3 = np.sqrt(r2**2 - r1**2) #5
-phi2 = np.arccos((a3**2-a2**2-r2**2)/(-2*a2*r3)) #6
+if r1 == 0:
+    phi1 = np.pi/2 if r2 > 0 else - np.pi/2
+else:
+    phi1 = np.arctan(r2/r1) #4
+
+r3 = np.sqrt(r2**2 + r1**2) #5
+
+phi2 = np.arccos(np.clip((a3**2-a2**2-r3**2)/(-2*a2*r3),-1,1)) #6
 
 Th2 = (phi1 + phi2) * 180/np.pi #7
 
-phi3 =  np.arccos((r3**2-a2**2-a3**2)/(-2*a2*a3)) #8
+phi3 =  phi3 = np.arccos(np.clip((r3**2-a2**2-a3**2)/(-2*a2*a3),-1,1)) #8
 
-Th3 = (phi3*180/np.pi) - 180 #9
+Th3 = (phi3-np.pi) * 180/np.pi #9
 
 print("Th1 = ", np.around(Th1,3))
 
